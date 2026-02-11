65872e5f0bde180bcd02b5cb7b58b4ce2ab8d8ca
Erick <erick.galdino@gfxconsultoria.com>
2026-02-11 16:59:33 -0300

    fazendo rotas

--- miniprojeto/backend/src/adpter/inbound/routes/routesUser.ts
+++ miniprojeto/backend/src/adpter/inbound/routes/routesUser.ts
@@ -5,3 +5,12 @@ import {makeCreateUser} from '../../../main/factory/factoryCreateUser'
 import {makeDeleteUser} from '../../../main/factory/factoryDeleteUser'
 import {makeLoginUser} from '../../../main/factory/factoryLoginUser'
 import {makeUpdateUser} from '../../../main/factory/factoryUpdateUser'
+
+const router = Router();
+
+const createControler = makeCreateUser();
+//const createControler = makeByAllUser();
+//const createControler = makeByIdUser();
+//const createControler = makeDeleteUser();
+//const createControler = makeLoginUser();
+//const createControler = makeUpdateUser();
