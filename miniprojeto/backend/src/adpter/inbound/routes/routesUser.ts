49cbfa5a4a641c835c85ebbafda62368c80e5ea3
Erick <erick.galdino@gfxconsultoria.com>
2026-02-11 16:57:51 -0300

    fazendo rotas

--- miniprojeto/backend/src/adpter/inbound/routes/routesUser.ts
+++ miniprojeto/backend/src/adpter/inbound/routes/routesUser.ts
@@ -1,1 +1,7 @@
 import { Router } from 'express';
+import {makeByAllUser} from '../../../main/factory/factoryByAllUser'
+import {makeByIdUser} from '../../../main/factory/factoryByIdUser'
+import {makeCreateUser} from '../../../main/factory/factoryCreateUser'
+import {makeDeleteUser} from '../../../main/factory/factoryDeleteUser'
+import {makeLoginUser} from '../../../main/factory/factoryLoginUser'
+import {makeUpdateUser} from '../../../main/factory/factoryUpdateUser'
