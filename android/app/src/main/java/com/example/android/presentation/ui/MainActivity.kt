da7296315c00cea069dd584fc7edf718e60127b9
Erick <erick.galdino@gfxconsultoria.com>
2026-02-19 15:20:56 -0300

    telas do android

--- android/app/src/main/java/com/example/android/presentation/ui/MainActivity.kt
+++ android/app/src/main/java/com/example/android/presentation/ui/MainActivity.kt
@@ -1,15 +1,16 @@
-package com.example.android
+package com.example.android.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.android.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
     }
+
 }
